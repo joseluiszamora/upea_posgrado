b37abf5a23f70d77747466d9ef411588d49480e9
jzamora <jzamora@miteleferico.bo>
2025-04-01 12:03:34 -0400

    page lista de cursos

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -15,6 +15,9 @@
 // }
 
 import Link from "next/link";
+import { courseData } from "@/app/api/data";
+import Image from "next/image";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 // Simulación de datos de cursos
 const cursos = [
@@ -40,22 +43,83 @@ const cursos = [
 
 export default function CursosPage() {
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-6">Nuestros Cursos</h1>
+    <section id="courses">
+      <div className="container mx-auto lg:max-w-screen-xl md:max-w-screen-md px-10 py-10">
+        <div className="sm:flex justify-between items-center mb-20">
+          <h2 className="text-midnight_text text-4xl lg:text-5xl font-semibold mb-5 sm:mb-0">
+            Oferta Académica
+          </h2>
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {cursos.map((curso) => (
-          <Link href={`/cursos/${curso.slug}`} key={curso.id}>
-            <div className="border rounded-lg p-6 hover:shadow-lg transition-shadow cursor-pointer">
-              <h2 className="text-xl font-semibold mb-2">{curso.titulo}</h2>
-              <p className="text-gray-600">{curso.descripcionCorta}</p>
-              <div className="mt-4 text-blue-600 font-medium">
-                Ver detalles →
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {courseData.map((items, i) => (
+            <div key={i}>
+              <div className="bg-white m-3 mb-12 px-3 pt-3 pb-12 shadow-course-shadow rounded-2xl h-full">
+                <div className="relative rounded-3xl">
+                  <Image
+                    src={items.imgSrc}
+                    alt="course-image"
+                    width={389}
+                    height={262}
+                    className="m-auto clipPath"
+                  />
+                </div>
+
+                <div className="px-3 pt-6">
+                  <Link
+                    href="#"
+                    className="text-2xl font-bold text-black max-w-75% inline-block"
+                  >
+                    {items.heading}
+                  </Link>
+                  <h3 className="text-2xl font-normal pt-6 text-black/75">
+                    {items.name}
+                  </h3>
+
+                  <div className="flex justify-between items-center py-6 border-b">
+                    {/* <div className="flex items-center gap-4">
+                      <h3 className="text-red-700 text-2xl font-medium">
+                        {items.rating}
+                      </h3>
+                      <div className="flex">
+                        {renderStars(items.rating, i)}
+                      </div>
+                    </div> */}
+                    <h3 className="text-xl font-medium">
+                      Inicia el {items.dateStart}
+                    </h3>
+                  </div>
+
+                  <div className="flex justify-between pt-6">
+                    <div className="flex gap-4">
+                      <Icon
+                        icon="solar:notebook-minimalistic-outline"
+                        className="text-primary text-xl inline-block me-2"
+                      />
+                      <h3 className="text-base font-medium text-black opacity-75">
+                        {items.classes} semanas
+                      </h3>
+                    </div>
+                    <div className="flex gap-4">
+                      {/* <Icon
+                        icon="solar:users-group-rounded-linear"
+                        className="text-primary text-xl inline-block me-2"
+                      /> */}
+                      <h3 className="text-base font-medium text-black opacity-75">
+                        Modalidad Virtual
+                      </h3>
+                    </div>
+                  </div>
+
+                  {/* <div className="mt-4 text-blue-600 font-medium">
+                    Ver detalles →
+                  </div> */}
+                </div>
               </div>
             </div>
-          </Link>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -2,8 +2,8 @@ import { HeaderItem } from "@/types/menu";
 
 export const headerData: HeaderItem[] = [
   { label: "Inicio", href: "/" },
-  { label: "Cursos", href: "/#courses  " },
-  { label: "Docentes", href: "/#mentor" },
-  { label: "Group", href: "/#portfolio" },
-  { label: "Testimonial", href: "/#testimonial" },
+  { label: "Oferta Académica", href: "/#courses  " },
+  { label: "Nuestros Docentes", href: "/#mentor" },
+  // { label: "Group", href: "/#portfolio" },
+  // { label: "Testimonial", href: "/#testimonial" },
 ];
